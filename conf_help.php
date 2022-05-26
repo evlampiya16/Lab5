0a25e55a6a710e39f17175123ccdd1f2abd0e9aa
evlampiya <eva.shamgunova@mail.ru>
2022-05-26 18:15:11 +0200

    End lab

---
+++ conf_help.php
@@ -0,0 +1,12 @@
+<?php
+
+
+function buildTask(array $taskData): array
+{
+    return [
+        $taskData['CATEGORY'],
+        $taskData['TITLE'],
+        $taskData['EMAIL'],
+        $taskData['TEXT']
+    ];
+} 
